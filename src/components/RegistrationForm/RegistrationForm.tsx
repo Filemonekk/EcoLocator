af71b25b3097c2219967f893939d79a2f926cce8
KasiaH <kah.gda@wp.pl>
2025-01-21 14:44:56 +0100

    updated regitration form

--- src/components/RegistrationForm/RegistrationForm.tsx
+++ src/components/RegistrationForm/RegistrationForm.tsx
@@ -1,49 +1,54 @@
+import { useState } from 'react';
 import Button from '../Button/Button'
 import styles from './RegistrationForm.module.scss';
-
-
-{/*const RegistrationForm = () => {
-    return (
-      
-      <form className={styles.formStyle}>
-          <div >
-              <label htmlFor='email'>E-mail</label>
-              <input id='email' type='email'/>
-          </div>
-          <div>
-              <label htmlFor='password'>Password</label>
-              <input id='password' type='password'/>
-          </div>
-          <Button label='Send' type='submit'/>
-            </form>
-           
-  )
-}
-
-export default RegistrationForm*/}
 
 interface RegistrationFormProps {
   className?: string; // Opcjonalny className
 }
 
 const RegistrationForm: React.FC<RegistrationFormProps> = ({ className }) => {
+  const [formData, setFormData] = useState({
+    companyName: '',
+    companyId: '',
+    city: '',
+    zipCode: '',
+    registrationPassword: '',
+    confirmRegistrationPassword: '',
+  });
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Wysłane dane:", formData);
+    if (formData.registrationPassword !== formData.confirmRegistrationPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+    alert(`Zarejestrowano użytkownika: ${formData.companyName}`);
+  };
+
+
+
   return (
-    <form className={styles.formStyle}>
+    <form onSubmit={handleSubmit} className={styles.formStyle}>
       <label>Company name</label>
-      <input type='text' name='companyName' />
+      <input type='text' name='companyName' value={formData.companyName} onChange={handleInputChange} />
       <label>Company ID</label>
-      <input type='number' name='companyId' />
+      <input type='number' name='companyId' value={formData.companyId} onChange={handleInputChange} />
       <label>City</label>
-      <input type='text' name='city' />
+      <input type='text' name='city' value={formData.city} onChange={handleInputChange} />
       <label>Zip-Code</label>
-      <input type='number' name='ZipCode' />
+      <input type='number' name='zipCode' value={formData.zipCode} onChange={handleInputChange} />
       <label>Password</label>
-      <input type='password' name='registrationPassword' />
+      <input type='password' name='registrationPassword' value={formData.registrationPassword} onChange={handleInputChange} />
       <label>Confirm Password</label>
-      <input type='password' name='confirmRegistrationPassword'/>
-      <Button label='Register' />
-      
-      
+      <input type='password' name='confirmRegistrationPassword' value={formData.confirmRegistrationPassword} onChange={handleInputChange} />
+      <Button type='submit' label='Register' />
+
     </form>
   )
 }
