f1018c5c0c83d26a0d674b7a2677b8c05f00bfb9
KasiaH <kah.gda@wp.pl>
2025-02-03 15:06:33 +0100

    created WasteForm using Bootstrap

--- src/pages/EnteringOrders.tsx
+++ src/pages/EnteringOrders.tsx
@@ -1,10 +1,17 @@
+import styles from './EnteringOrders.module.scss'
 import PageNav from '../components/PageNav/PageNav'
+import WasteForm from '../components/WasteForm/WasteForm'
 
 const EnteringOrders: React.FC =() => {
 	return (
 		<div>
 			<PageNav />
-			<div> Formula</div>
+			<div className='text'>
+				<h1>Please fill the information</h1>
+			</div>
+			<div className={styles.wasteForm}>
+				<WasteForm />
+				</div>
 		</div>
 	)
 }
--- src/pages/SearchingOrders.tsx
+++ src/pages/SearchingOrders.tsx
@@ -2,16 +2,25 @@ import PageNav from '../components/PageNav/PageNav'
 import Sidebar from '../components/Sidebar/Sidebar'
 import styles from './SearchingOrders.module.scss'
 import Map from '../components/Map/Map'
+import WasteCard from '../components/WasteCard/WasteCard'
 
-
-const SearchingOrders: React.FC = () => {
+function SearchingOrders() {
 	return (
 		<>
 			<PageNav />
-			<div className={styles.side}>
-				<Sidebar title='Formula'/>
-				<Map titlemap='Map' />
-			</div>
+			<main className={styles.container}>
+				<aside className={styles.sidebar}>
+					<Sidebar title='Formula' />
+				</aside>
+				<section className={styles.content}>
+					<div className={styles.map}>
+						<Map titlemap='Map' />
+					</div>
+					<div className={styles.wasteCard}>
+					<WasteCard />
+					</div>
+				</section>
+			</main>
 		</>
 	)
 }
