09fe301707a4ceb4347292a08013035a989e9d8f
KasiaH <kah.gda@wp.pl>
2025-02-05 15:36:10 +0100

    correctly installed react query and react query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './App.scss';
 import HomePage from './pages/HomePage';
 import Admin from './pages/Admin';
@@ -9,6 +11,15 @@ import EnteringOrders from './pages/EnteringOrders';
 import SearchingOrders from './pages/SearchingOrders';
 import PageNotFound from './pages/PageNotFound';
 import Register from './pages/Register';
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    }
+  }
+})
 
 const router = createBrowserRouter([
   {
@@ -68,9 +79,14 @@ const router = createBrowserRouter([
   },
 ]);
 
-function App() {
-  return <RouterProvider router={router} />;
-}
+const App: React.FC = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false}/>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
+};
 
 export default App;
 
