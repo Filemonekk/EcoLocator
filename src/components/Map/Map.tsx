a5b1153f0fa8b085e0261601cd5402c8396896ae
KasiaH <kah.gda@wp.pl>
2025-03-31 12:25:54 +0200

    installed leaflet, add map to map.tsx and styles

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,13 +1,37 @@
-import styles from './Map.module.scss'
-
-import React from 'react'
+import React, { useEffect } from 'react';
+import L from 'leaflet';
+import styles from './Map.module.scss';
+import 'leaflet/dist/leaflet.css';
 
 interface Props {
-	titlemap: string
+  titlemap: string;
 }
 
 const Map: React.FC<Props> = ({ titlemap }) => {
-	return <h1 className={styles.titlemap}>{titlemap}</h1>
-}
+  useEffect(() => {
+    const map = L.map('map').setView([54.3520252, 18.6466384], 13);
 
-export default Map
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
+
+    L.marker([54.3520252, 18.6466384])
+      .addTo(map)
+      .bindPopup('Object')
+      .openPopup();
+
+    // Cleanup on unmount
+    return () => {
+      map.remove();
+    };
+  }, []);
+
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.titlemap}>{titlemap}</h1>
+      <div id="map" className={styles.mapContainer}></div>
+    </div>
+  );
+};
+
+export default Map;
